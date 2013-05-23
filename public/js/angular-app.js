d601b6e8cb4d885489111f947be4a1d2565b4bd9
Varun Vachhar <varunvachhar@gmail.com>
2013-05-23 19:10:00 -0400

    added polygonsExporter to nav

--- public/js/angular-app.js
+++ public/js/angular-app.js
@@ -3,7 +3,7 @@
 /*************************************
   APP
 *************************************/
-angular.module('exportPolygons', ['getShapes']);
+var app = angular.module('exportPolygons', ['getShapes']);
  
 
  
@@ -101,6 +101,124 @@ function ShapeCtrl ($scope, Shapes) {
     return len;
   };
 }
+
+
+
+/*************************************
+  DIRECTIVES
+*************************************/ 
+app.directive('polygon', function() {
+  return {
+    restrict: 'A',
+    scope: { 
+      vertices: '=polygon',  
+    },
+    link: function(scope, element, attrs)
+    {
+      var camera, scene, renderer;
+      var polygon;
+      var targetRotation = 0;
+      var targetYRotation = 0, targetXRotation = 0;
+      var targetYRotationOnMouseDown = 0, targetXRotationOnMouseDown = 0;
+      var mouseX = 0, mouseY = 0;
+      var mouseXOnMouseDown = 0, mouseYOnMouseDown = 0;
+      var width = $(element).width();
+      var height = 200;
+      var widthHalfX = width/2;
+      var widthHalfY = height/2;
+
+      init();
+      animate();
+
+      function init() {
+        // Setup scene
+        camera = new THREE.PerspectiveCamera( 70, width / height, 1, 1000 );
+        camera.position.x = 0;
+        camera.position.y = 0;
+        camera.position.z = 300;
+        scene = new THREE.Scene();
+        // Build Polygon
+        var geometry =  new THREE.Geometry();
+        angular.forEach(scope.vertices, function (v) {
+          geometry.vertices.push( new THREE.Vector3( v.x, v.y, v.z ) );
+        });
+        // Build front and back 
+        if (geometry.vertices.length===4) { 
+          geometry.faces.push( new THREE.Face4(0, 1, 2, 3) );
+        } else {
+          geometry.faces.push( new THREE.Face3(0, 1, 2 ));
+        }
+        THREE.GeometryUtils.center( geometry );
+
+        // var c = new THREE.Vector3();
+        // var nPolygon = new THREE.Vector3();
+        // nPolygon.crossVectors(c.crossVectors( geometry.vertices[0], geometry.vertices[1] ), geometry.vertices[2]);
+
+        // if (nPolygon) {
+        //   var nPolygonX = new THREE.Vector3(nPolygon.x,0,0);
+        //   var xRot = Math.acos(nPolygonX.dot( new THREE.Vector3(1,0,0) )/nPolygonX.length() ); 
+        //   var nPolygonY = new THREE.Vector3(0,nPolygon.y,0);
+        //   var yRot = Math.acos(nPolygonY.dot( new THREE.Vector3(1,0,0) )/nPolygonY.length() ); 
+        //   var nPolygonZ = new THREE.Vector3(0,0,nPolygon.z);
+        //   var zRot = Math.acos(nPolygonZ.dot( new THREE.Vector3(1,0,0) )/nPolygonZ.length() ); 
+        // } 
+
+        // geometry.applyMatrix( new THREE.Matrix4().makeRotationX(xRot) );
+        // geometry.applyMatrix( new THREE.Matrix4().makeRotationY(yRot) );
+        // geometry.applyMatrix( new THREE.Matrix4().makeRotationZ(zRot) );
+
+
+        geometry.applyMatrix( new THREE.Matrix4().makeRotationX(Math.PI/2) );
+        // Push polygon to scene
+        var material = new THREE.MeshBasicMaterial( { color: 0x2ECC71, side: THREE.DoubleSide } );
+        polygon = new THREE.Mesh( geometry, material );
+        scene.add(polygon);
+
+        renderer = new THREE.WebGLRenderer(); // new THREE.CanvasRenderer();
+        renderer.setSize( width, height );
+        $(element).append( renderer.domElement );
+        $(element).on('mousedown', onDocumentMouseDown);
+      }
+      // MOUSE EVENTS
+      function onDocumentMouseDown( event ) {
+        event.preventDefault();
+        $(element).on('mousemove', onDocumentMouseMove);
+        $(element).on('mouseup', onDocumentMouseUp);
+        $(element).on('mouseout', onDocumentMouseOut);
+        mouseXOnMouseDown = event.clientX - widthHalfX;
+        mouseYOnMouseDown = event.clientY - widthHalfY;
+        targetYRotationOnMouseDown = targetYRotation;
+        targetXRotationOnMouseDown = targetXRotation;
+      }
+      function onDocumentMouseMove( event ) {
+        mouseX = event.clientX - widthHalfX;
+        mouseY = event.clientY - widthHalfY;
+        targetYRotation = targetYRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
+        targetXRotation = targetXRotationOnMouseDown + ( mouseY - mouseYOnMouseDown ) * 0.02;
+      }
+      function onDocumentMouseUp( event ) {
+        $(element).off('mousemove', onDocumentMouseMove);
+        $(element).off('mouseup', onDocumentMouseUp);
+        $(element).off('mouseout', onDocumentMouseOut);
+      }
+      function onDocumentMouseOut( event ) {
+        $(element).off('mousemove', onDocumentMouseMove);
+        $(element).off('mouseup', onDocumentMouseUp);
+        $(element).off('mouseout', onDocumentMouseOut);
+      }
+      // UPDATE AND RENDER
+      function animate() {
+        requestAnimationFrame( animate );
+        render();
+      }
+      function render() {
+        polygon.rotation.x += ( targetXRotation - polygon.rotation.x ) * 0.05;
+        polygon.rotation.y += ( targetYRotation - polygon.rotation.y ) * 0.05;
+        renderer.render( scene, camera );
+      }
+    }
+  };
+});
 
 
 
